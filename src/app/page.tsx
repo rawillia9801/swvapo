dd1d2334960d3b663dc6a6426c0fb5b1e52f7377
rawillia9801 <163735923+rawillia9801@users.noreply.github.com>
2026-08-01 10:45:21 -0400

    Redirect portal domain to the public SWVAOS portal login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
 
 export default function HomePage() {
-  redirect("https://www.swvaos.site/portal/login");
+  redirect("https://swvaos.site/portal/login");
 }
